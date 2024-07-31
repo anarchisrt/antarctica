2df9e1be58324c867693b1ccf96ab23cdc57e8fd
anarchisrt <105643886+anarchisrt@users.noreply.github.com>
2024-07-31 21:02:57 +0200

    Add files via upload

---
+++ roll_resolver.lua
@@ -0,0 +1,73 @@
+--⢐⠈⠄⢂⢐⠐⡐⠠⠈⠄⢂⠐⠄⡁⢂⢐⢬⣿⢿⣽⢿⣽⣻⡽⡿⣵⣻⣺⣺⣳⡳⣕⢧⡣⡐⠄⢂⠄⡂⠄⠂⡀⢂⠐⡀⢂⢁⠐⡀⠢⠐⡀⢂⠨⢐⢔⢑⢅⠣⡃⡇⡣⡪⣪⢪⢎
+--⢀⠊⠨⠠⠐⢐⠠⠁⠡⠈⠄⠠⠡⢐⢀⢢⡿⣾⢿⡯⣿⢽⢾⢽⢽⣺⣺⣺⣺⡺⣮⡳⣫⢞⡎⡆⠄⠂⠠⠐⠄⠂⡀⢂⠐⠠⠐⢀⠂⡁⠂⠂⡂⢐⠐⢌⠢⡡⡃⢇⢕⢕⢕⢕⢕⢕
+--⢐⠨⠈⠄⠅⢂⠐⡈⠄⠅⠌⣘⢌⢐⠌⡓⣽⣻⣽⢯⡯⣟⡽⣽⡳⣗⣗⣗⢷⢽⣺⡺⣪⢧⢳⡹⡹⡠⡅⠠⠁⡂⢐⠄⢂⠡⠈⡀⢂⠠⠁⡁⠄⠂⡈⡢⠱⡐⡅⡣⡱⡸⡸⡰⡱⡣
+--⢐⠈⠌⠄⠅⢂⠐⠠⠈⠄⢂⢺⣢⡣⡕⣼⣺⣞⣗⡯⣯⣗⣯⢷⣻⣽⢾⢾⣝⡗⣗⣝⢮⡳⡹⣪⢺⢸⢜⢜⢔⢔⠠⡈⠄⡐⢈⠠⢀⠄⡂⠄⠌⠠⠐⢌⢊⠢⡱⢨⠢⡃⡇⡇⡗⣝
+--⠠⠨⠠⠁⡂⢂⠨⠠⠁⠅⠂⠌⣺⡜⣮⣿⢾⣳⡯⣟⣾⣺⣽⢯⣟⡾⣝⣗⢵⢝⣞⢼⢕⡝⡎⣎⢗⡝⣎⢗⢵⢱⠱⡱⡱⡸⡸⡸⡐⡅⢌⠌⠌⠄⢡⠱⢨⠨⢜⢰⢑⢕⢕⢜⢜⢆
+--⠐⡈⠄⠡⠐⡀⢂⠡⢨⡼⢶⣧⣔⣿⣽⣾⢿⡯⣯⢷⣗⣟⣞⢟⢎⡯⣺⣪⢗⡧⡳⡕⡇⡇⡏⡎⡇⡏⡎⡇⡇⡣⡣⣳⢱⢕⡕⡜⢔⠜⡐⠅⠅⡈⡢⡑⡅⢕⠱⡐⢕⢌⢎⠎⡎⡎
+--⠠⠐⠈⢄⠡⠐⡐⠨⢸⠇⢕⢹⡻⣽⣯⣿⣟⣿⢽⣻⡺⣺⢺⢝⠳⡑⢑⠙⠩⠓⠇⡇⠧⡱⠡⢣⠱⡱⡱⠱⡸⠸⠪⡣⡫⡪⡪⡪⡑⢕⠨⡨⠂⠌⡢⡊⡢⢡⢑⢅⢣⢑⢅⢇⢏⢎
+--⠠⠁⠅⢂⠐⡁⠄⠡⠘⣇⢇⢷⡵⡝⢷⣿⣽⡾⣿⢽⣮⠾⠑⠣⣑⠈⠄⣐⡐⠠⠡⠠⡑⣜⡸⡰⡁⡃⠄⢁⠠⠈⠄⡀⠐⠐⢐⠠⠑⡁⠢⡊⠄⡑⠐⣌⠌⡆⠕⢌⠢⡑⡅⡣⡱⡱
+--⠠⠁⠅⢂⢂⢐⠈⠌⡐⢹⡬⣗⢇⢦⡿⣷⣟⣟⣯⣗⡥⣼⣼⡮⡻⡤⡦⡺⣑⠱⡐⠴⡸⣸⢾⢕⢌⠄⡆⡡⡢⢿⡠⠁⣌⢗⠠⢈⠢⠨⡂⠪⢐⠠⢹⢎⠪⡐⢕⠡⡃⡪⡐⡕⢜⢜
+--⠈⠌⠌⡐⡀⠢⠨⠐⠨⢀⢻⣺⣕⡽⡿⣯⡿⣽⢷⡷⣟⣗⡯⡫⣓⠇⡗⢌⠢⡑⣌⢮⣪⢾⢯⡣⡃⢂⢗⡅⡕⢕⠭⢭⠨⡢⠡⡂⢌⢊⠌⠌⠄⠂⢝⠨⡊⢌⠆⡣⠪⡐⡱⡨⡊⡆
+--⠨⠠⢁⠂⠄⡑⢈⠨⠐⢐⠘⣯⣿⢷⡟⣯⡿⣯⢿⣻⡯⣟⡾⣽⡪⡎⡎⡆⣕⣺⢮⣗⡯⣟⡷⡕⢕⢐⠐⡕⡕⣑⢑⠅⡕⢌⢪⢰⡑⡌⠌⢂⠡⡈⠆⠕⡌⡢⢱⠨⡪⢨⠢⢪⢘⠬
+--⢈⠂⡂⠅⢂⠂⡂⠄⠅⡂⠌⢹⣟⡧⣯⢿⣽⣯⡯⣗⢯⡳⡻⣚⣎⣗⣵⣫⢞⡕⣏⣞⢮⣯⢿⡪⡂⡂⠅⡊⡪⣒⢬⢒⢌⢆⠧⡱⡨⠂⠅⠂⡂⢌⠪⡨⠢⡊⢆⠕⡌⡢⢣⢑⢅⢇
+--⢐⠐⡀⠅⡂⠂⠄⠅⡂⠔⢈⠠⠙⠝⠝⡸⣷⣯⢿⣝⡮⣺⡪⣗⣗⢗⡗⡇⡇⡑⣜⡾⣽⢾⡻⡜⢔⠠⡁⢜⢜⡜⣎⢧⢳⢑⢱⢈⠢⠡⠡⢈⠐⢅⠕⡘⡌⡌⢆⠕⡌⡊⡆⡣⡊⡆
+--⢐⠐⡐⠐⡈⠌⡨⠐⠠⠈⠄⡐⠠⢁⠐⠄⣿⣾⢿⣳⢯⣳⢯⢗⡗⣝⢜⢎⢢⢸⣷⢿⣟⡿⣝⣎⠢⡁⠢⠘⢜⢜⢜⢜⢌⠪⡐⢌⠌⢌⢐⠠⡑⢅⢪⠨⡢⠪⡊⡪⡘⡌⡪⡨⡪⢪
+--⠠⡁⡂⡁⡂⠅⡂⠡⢁⢁⢂⢐⢁⢂⠡⡁⢺⣿⣻⣽⣳⢯⢯⡳⣝⢮⡣⣣⣳⡝⠍⠊⠚⡪⠪⠸⠐⠠⠁⠅⡪⡣⢣⢑⢅⢣⢘⠔⡡⢂⠢⡱⠨⢢⢑⢕⢘⠜⡌⢎⢪⢸⢨⢢⢣⢣
+--⠨⡐⡐⢐⢈⠢⡈⡂⡂⢂⢐⠠⠂⠔⡐⠨⢨⣿⣽⢾⣺⢽⢕⣟⢮⢷⢽⣳⡳⡡⠐⡈⠄⠂⠨⠄⠅⢀⠁⢅⠣⡣⡑⢥⢑⢅⠕⠌⠔⡈⡢⡊⡪⠢⡱⡐⢅⢣⢊⢎⢪⢢⢣⢱⢸⢰
+--⠨⢂⠂⠅⡂⡑⡐⠔⡠⢁⠂⠌⠌⠌⢄⠕⣽⢿⡾⣯⢯⢯⢯⢾⢽⣻⢿⡽⣣⠡⠂⡀⠊⠄⠑⡈⡀⢂⠐⢐⢱⢑⠜⢌⢎⠢⡑⡡⢱⣌⣆⠪⡐⢕⢌⢊⡪⢸⠰⡱⡑⡕⡅⡇⡇⢧
+--⠨⡂⢅⢑⠨⡐⡡⢑⠄⡑⠌⠌⡊⢌⠢⣸⣿⢽⢯⣯⢷⣫⢿⡽⡿⡽⠹⢘⢑⠣⡃⡂⡅⡅⢅⢂⠔⡐⡈⡐⢌⠢⡃⡣⡱⠡⠢⡑⢈⢖⡔⡡⢝⣜⢶⣵⣜⣌⢎⢆⢣⢣⢣⢣⢫⢪
+--⠨⡐⢔⢐⢁⠢⢂⠅⡌⠔⡁⠅⡂⠢⣱⣿⣟⣇⢿⣾⢽⣺⢽⣺⡹⡸⣸⢼⣝⣯⢿⠝⠝⡩⡃⡇⡇⡒⢌⢢⢡⢑⢕⢱⢈⢊⠪⠐⠠⠨⠘⢔⢱⣹⡲⡮⣾⣜⢿⣾⣮⣎⣎⢎⣎⢧
+--⠨⡂⡢⢂⠢⢡⠑⢌⢐⢁⢂⢅⣦⡿⣿⡯⣿⣽⡜⡯⣟⢞⢽⡪⣎⢏⢎⢇⠇⢇⠣⠡⡑⢌⠪⡸⡐⢕⠱⡑⢌⠢⠣⡑⢔⠡⢁⠊⡨⠠⠡⢡⠑⢮⡺⣝⣞⢾⢝⣞⣿⣻⣿⣿⣾⣾
+--⠨⡂⡢⢑⠌⡢⢑⢅⠢⢂⣖⣿⣾⢿⣯⢿⢵⣳⢯⢮⡱⢯⡪⡫⡪⣳⢱⣣⡳⡵⣝⡮⣟⢾⢝⣎⠮⡪⢪⢘⢌⠪⡨⡈⠢⠈⠄⡂⡂⡑⡈⡂⢅⠣⡹⠵⣕⢗⣝⢖⢎⣷⣿⣾⡿⣾
+--⢘⠔⡨⠢⡑⡐⢅⢢⣱⣿⠿⣽⣫⣿⡽⣝⢕⣯⡺⣪⢪⢮⡹⡺⣌⢗⢝⢮⢞⢽⡪⢯⠺⡩⢪⠪⡊⠜⡌⢆⠅⠕⡠⠨⠄⠅⡊⢄⢂⠢⡐⠨⡂⠕⡱⠱⡱⡳⡕⡏⡗⡜⡻⣝⢞⣿
+--⢑⢌⠢⡑⢔⢌⣶⢿⣻⡝⣸⡏⢮⣾⣻⡪⢪⣺⢮⣳⢱⢕⢝⢮⡪⡫⢮⡣⡫⢊⠪⠨⡊⢌⠢⡑⠜⢌⠢⢑⢈⢂⢐⠠⠡⡑⡐⢅⠪⡐⢌⢌⠢⡑⢜⢸⢘⢜⢵⢹⡪⡮⡪⡪⡯⣿
+
+local ffi = require('ffi')
+local uilib = require("gamesense/uilib")
+local plist_handler = uilib.create_plist()
+local roll_resolver = plist_handler:add(ui.new_slider, 'Roll resolver', 0, 90, 0)
+
+local ientitylist = ffi.cast('void***', client.create_interface('client.dll', 'VClientEntityList003'))
+local get_client_entity = ffi.cast('void*(__thiscall*)(void*, int)', ientitylist[0][3])
+
+local function set_roll(player, roll)
+    ffi.cast('float*', ffi.cast("char*", get_client_entity(ientitylist, player)) + 0x117D8)[0] = roll
+end
+
+local misses = {}
+client.set_event_callback('aim_miss', function(e) 
+    if e.reason ~= '?' then
+        return
+    end
+
+    if not misses[e.target] then
+        misses[e.target] = 0
+    end
+
+    misses[e.target] = misses[e.target] + 1
+end)
+
+client.set_event_callback('player_connect', function(e) 
+    misses[client.userid_to_entindex(e.userid)] = 0
+end)
+
+client.set_event_callback('net_update_start', function()
+    local enemies = entity.get_players(true)
+    for _, player in ipairs(enemies) do
+        if player and entity.is_alive(player) then
+            if not misses[player] then
+                misses[player] = 0
+            end
+
+            local roll = plist_handler:get_state(roll_resolver, player)
+            if roll ~= 0 then
+                plist.set(player, 'Force body yaw', true)
+                plist.set(player, 'Force body yaw value', 60 * ((misses[player] % 2) * 2 - 1))
+            else
+                plist.set(player, 'Force body yaw', false)
+            end
+
+            set_roll(player, roll * (misses[player] % 2) * 2 - 1)
+        end
+    end
+end)
